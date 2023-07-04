09eddeb4b7cab8cda7d251ddfaab69e5fe8104b9
MaksKorhun <folded35@gmail.com>
2023-07-04 23:46:53 +0200

    add something

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  email: document.querySelector('[name="email"]'),
+  textarea: document.querySelector('[name="message"]'),
+  form: document.querySelector('form'),
+};
+
+const FEEDBACK_DATA = 'feedback-form-state';
+let feedbackData = JSON.parse(localStorage.getItem(FEEDBACK_DATA)) || {};
+
+const recoverData = () => {
+  refs.email.value = feedbackData.email || '';
+  refs.textarea.value = feedbackData.message || '';
+};
+
+recoverData();
+
+const updateFeedbackData = event => {
+  feedbackData[event.target.name] = event.target.value;
+  localStorage.setItem(FEEDBACK_DATA, JSON.stringify(feedbackData));
+};
+const onFormSubmitting = event => {
+  event.preventDefault();
+
+  console.log(feedbackData);
+
+  localStorage.removeItem(FEEDBACK_DATA);
+  event.currentTarget.reset();
+  feedbackData = {};
+};
+
+refs.form.addEventListener('input', throttle(updateFeedbackData, 500));
+refs.form.addEventListener('submit', onFormSubmitting);
